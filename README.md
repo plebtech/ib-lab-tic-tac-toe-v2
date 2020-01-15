bb0d885c481cf1d1cf5b3fafa44564acf96cad48
Nick Mc <nbmc@protonmail.com>
2020-01-15 16:31:17 -0600

    added readme, struggling with game logic.

--- README.md
+++ README.md
@@ -8,3 +8,15 @@
 * after each move, board is checked for winner or a draw.
     * if won/draw, show message announcing winner/draw (status).
 * when game is over, click board to reset/restart.
+
+### approach:
+* add a status div to display the game's current status (ready, turn number, winner/draw).
+    * selector for 'status' div.
+    * updateStatus function to update, called on every click.
+* check if the game has been won:
+    * check rows to see if three cells are identical.
+    * check columns.
+    * check diagonal.
+    * switch block?
+* check for draw:
+    * all cells clicked, game not won.
--- app.js
+++ app.js
@@ -1,28 +1,53 @@
+/* TO DO:
+- populate an array to represent the game board, iterating through with every click to populate/update it.
+- restart gameboard when game is over.
+*/
+
+// winning combinations to test against.
+const WINNING_COMBOS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,4,8],
+    [2,4,6],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8]
+];
+
 // selector for the status div.
 let status = document.querySelector('#status');
 // selector for all game 'cells'.
 let cells = document.querySelectorAll('.top, .middle, .bottom');
 
-
-// variable for whether game has been won.
+// variables for whether game has been won/drawn.
 let gameWon = false;
+let gameDraw = false;
 // variable for game winner (used in displaying game status).
 winner = undefined;
 // move count.
 let count = 0;
+// track which cell clicked.
+let cellID;
+
+let TL = document.getElementById("cZero")
 
 // listener for cells being clicked.
 cells.forEach(function(cell) {
     cell.addEventListener("click", cellClicked);
 });
 
+// MAIN METHOD.
 // handles cell being clicked.
 function cellClicked(e) {
+    cellID = e.target;
     if (checkCell(e) === false) {
         setXO(e);
         count++;
     }
     updateStatus();
+    checkDraw();
+    console.log(TL.innerHTML);
 }
 
 // checks if a cell has been clicked yet.
@@ -33,6 +58,7 @@ function checkCell(e) {
     }
     return clicked;
 }
+
 // sets the value of a clicked cell to X or O based on the turn number.
 function setXO(e) {
     if (count % 2 === 0) {
@@ -45,8 +71,17 @@ function setXO(e) {
 // update the status div.
 function updateStatus() {
     if (gameWon === true) {
-        status.textContent = "Game over!" + winner + " wins!";
+        status.textContent = "Game over!" + winner + " wins! Click to play again.";
+    } else if (gameDraw === true) {
+        status.textContent = "Game is a draw! Click to play again.";
     } else if (count > 0) {
         status.textContent = "Game started! Turn number: " + count;
+    }
+}
+
+// checks to see if the game has ended in a draw.
+function checkDraw() {
+    if (gameWon === false && count === 9) {
+        status.textContent = "Draw! Game over. Click to play again.";
     }
 }
